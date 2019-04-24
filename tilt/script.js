3cbb775c7f75f3fe7c7689ee8a233bcac9b45f38
fenfil <kennyvova@ya.ru>
2019-04-24 23:00:36 +0300

    a

--- tilt/script.js
+++ tilt/script.js
@@ -1,11 +1,14 @@
 let a = document.querySelector('div');
+let {width: w, height: h} = window.getComputedStyle(a);
+w = parseFloat(w);
+h = parseFloat(h);
+a.style.width = w;
+a.style.height = h;
 
 window.addEventListener('deviceorientation', (e) => {
-    console.log(e);
-    
-    a.innerText = e.gamma;
-    b.innerText = e.beta;
-    c.innerText = e.alpha;
+    // a.innerText = e.gamma;
+    // b.innerText = e.beta;
+    // c.innerText = e.alpha;
+    a.height = h * Math.cos(e.gamma);
+    a.width = w * Math.cos(e.alpha);
 });
-
-let e = new DeviceOrientationEvent('deviceorientation');
