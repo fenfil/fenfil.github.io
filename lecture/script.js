ac838a79ab17b856aa847034beb1dad59ea6fb89
Vladimir Zaikovskii <kennyvova@ya.ru>
2022-12-14 21:17:34 +0300

    s

--- lecture/script.js
+++ lecture/script.js
@@ -27,20 +27,31 @@ class Particle {
   }
 
   draw() {
+    const d = Math.sqrt((this.x - mouseX) ** 2 + (this.y - mouseY) ** 2);
+
+    ctx.fillStyle = d > 50 ? "#aaaaaa" : "#cccccc";
+
     ctx.beginPath();
-    ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2);
+    ctx.arc(this.x, this.y, d > 50 ? this.r : this.r * 5, 0, Math.PI * 2);
     ctx.fill();
   }
 }
 
 let particles = new Array(200).fill(0).map(() => new Particle());
 
+let mouseX = 0;
+let mouseY = 0;
+
+document.addEventListener("mousemove", (e) => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+});
+
 function animate() {
   requestAnimationFrame(animate);
 
   ctx.fillStyle = "#ffffff";
   ctx.fillRect(0, 0, width, height);
-  ctx.fillStyle = "#aaaaaa";
 
   particles.forEach((particle) => {
     particle.run();
